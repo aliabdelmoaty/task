f22d6dd07cf981d6aacebca53ec1ed2ee6bf3c8e
Ali Abdelmoaty <68239507+aliabdelmoaty@users.noreply.github.com>
2023-11-02 12:34:52 +0200

    first commit

---
+++ lib/constants/assets.dart
@@ -0,0 +1,35 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesAccounting
+  /// assets/images/accounting.png
+  static const String imagesAccounting = "assets/images/accounting.png";
+
+  /// Assets for imagesBuy&sell
+  /// assets/images/buy&sell.png
+  static const String imagesBuySell = "assets/images/buy&sell.png";
+
+  /// Assets for imagesImage1
+  /// assets/images/Image1.png
+  static const String imagesImage1 = "assets/images/Image1.png";
+
+  /// Assets for imagesImage2
+  /// assets/images/Image2.png
+  static const String imagesImage2 = "assets/images/Image2.png";
+
+  /// Assets for imagesInsurances
+  /// assets/images/insurances.png
+  static const String imagesInsurances = "assets/images/insurances.png";
+
+  /// Assets for imagesLegal
+  /// assets/images/legal.png
+  static const String imagesLegal = "assets/images/legal.png";
+
+  /// Assets for imagesManPresentationMiniatureBuilding
+  /// assets/images/ManPresentationMiniatureBuilding.png
+  static const String imagesManPresentationMiniatureBuilding = "assets/images/ManPresentationMiniatureBuilding.png";
+
+  /// Assets for imagesVector
+  /// assets/images/Vector.png
+  static const String imagesVector = "assets/images/Vector.png";
+}
---
+++ lib/constants/constans.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+TextStyle textStyleOutfit = TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 20.sp,
+    fontFamily: 'Outfit',
+    color: const Color(0xff101010));
